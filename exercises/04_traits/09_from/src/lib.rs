c8f4a9269178004bc419d3120b8ee8a82989c413
Dboy <894230813@qq.com>
2024-10-24 21:48:44 +0800

    进行到第四章10节

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -1,7 +1,13 @@
-// TODO: Implement the `From` trait for the `WrappingU32` type to make `example` compile.
+// TODO: 为 'WrappingU32' 类型实现 'From' trait 以使 'example' 编译。
 
 pub struct WrappingU32 {
     value: u32,
+}
+
+impl From<u32> for WrappingU32 {
+    fn from(value: u32) -> Self {
+        WrappingU32 { value }
+    }
 }
 
 fn example() {
--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -1,17 +1,47 @@
-// TODO: Define a new trait, `Power`, that has a method `power` that raises `self`
-//  to the power of `n`.
-//  The trait definition and its implementations should be enough to get
-//  the tests to compile and pass.
+// TODO: 定义一个新的特征 'Power'，它有一个方法 'power' ，将 'self' 提升到 'n' 的幂。
+//  trait 定义及其实现应该足以使测试编译并通过。
 //
-// Recommendation: you may be tempted to write a generic implementation to handle
-// all cases at once. However, this is fairly complicated and requires the use of
-// additional crates (i.e. `num-traits`).
-// Even then, it might be preferable to use a simple macro instead to avoid
-// the complexity of a highly generic implementation. Check out the
-// "Little book of Rust macros" (https://veykril.github.io/tlborm/) if you're
-// interested in learning more about it.
-// You don't have to though: it's perfectly okay to write three separate
-// implementations manually. Venture further only if you're curious.
+// Recommendation: 您可能很想编写一个通用 implementation 来一次处理所有情况。然而
+//  这相当复杂，需要使用额外的 crate（即 'num-traits'）。
+//
+//即便如此，使用简单的宏可能更可取，以避免高度通用实现的复杂性。
+// 如果你有兴趣了解更多信息，请查看 “Little book of Rust macros” （https://veykril.github.io/tlborm/）。
+// 不过你不必这样做：手动编写三个单独的 implementations 是完全可以的。只有在你好奇的情况下才进一步冒险。
+
+
+// 定义 Power 特征
+//
+
+pub trait Power<Exponent = Self> {
+    type Output;
+
+    fn power(&self, n: Exponent) -> Self::Output;
+}
+
+impl Power<u16> for u32 {
+    type Output = u32;
+
+    fn power(&self, n: u16) -> Self::Output {
+        self.pow(n.into())
+    }
+}
+
+impl Power<&u32> for u32 {
+    type Output = u32;
+
+    fn power(&self, n: &u32) -> Self::Output {
+        self.power(*n)
+    }
+}
+
+impl Power<u32> for u32 {
+    type Output = u32;
+
+    fn power(&self, n: u32) -> Self::Output {
+        self.pow(n)
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
