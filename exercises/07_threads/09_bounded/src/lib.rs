75e8d6444df3f7bc57d185a80bd575da0cb25e2d
Dboy <894230813@qq.com>
2024-11-02 21:52:57 +0800

    7-11

--- exercises/07_threads/09_bounded/src/lib.rs
+++ exercises/07_threads/09_bounded/src/lib.rs
@@ -1,40 +1,49 @@
 // TODO: Convert the implementation to use bounded channels.
 use crate::data::{Ticket, TicketDraft};
 use crate::store::{TicketId, TicketStore};
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc::{channel, sync_channel, Receiver, RecvError, Sender, SyncSender};
 
 pub mod data;
 pub mod store;
 
 #[derive(Clone)]
 pub struct TicketStoreClient {
-    sender: todo!(),
+    sender: SyncSender<Command>,
 }
 
 impl TicketStoreClient {
-    pub fn insert(&self, draft: TicketDraft) -> Result<TicketId, todo!()> {
-        todo!()
+    pub fn insert(&self, draft: TicketDraft) -> Result<TicketId, RecvError> {
+        let channel = channel();
+        self.sender.send(Command::Insert {
+            draft,
+            response_channel: channel.0,
+        }).unwrap();
+        channel.1.recv()
     }
 
-    pub fn get(&self, id: TicketId) -> Result<Option<Ticket>, todo!()> {
-        todo!()
+    pub fn get(&self, id: TicketId) -> Result<Option<Ticket>, RecvError> {
+        let channel = channel();
+        self.sender.send(Command::Get { id, response_channel: channel.0 }).unwrap();
+        channel.1.recv()
     }
 }
 
 pub fn launch(capacity: usize) -> TicketStoreClient {
-    todo!();
-    std::thread::spawn(move || server(receiver));
-    todo!()
+    let channel = sync_channel(capacity);
+    std::thread::spawn(move || server(channel.1));
+    TicketStoreClient {
+        sender: channel.0
+    }
 }
 
-enum Command {
+pub enum Command {
     Insert {
         draft: TicketDraft,
-        response_channel: todo!(),
+        response_channel: Sender<TicketId>,
     },
     Get {
         id: TicketId,
-        response_channel: todo!(),
+        response_channel: Sender<Option<Ticket>>,
     },
 }
 
@@ -43,18 +52,18 @@ pub fn server(receiver: Receiver<Command>) {
     loop {
         match receiver.recv() {
             Ok(Command::Insert {
-                draft,
-                response_channel,
-            }) => {
+                   draft,
+                   response_channel,
+               }) => {
                 let id = store.add_ticket(draft);
-                todo!()
+                response_channel.send(id).unwrap();
             }
             Ok(Command::Get {
-                id,
-                response_channel,
-            }) => {
-                let ticket = store.get(id);
-                todo!()
+                   id,
+                   response_channel,
+               }) => {
+                let ticket = store.get(id).unwrap();
+                response_channel.send(Option::Some(ticket.clone())).unwrap()
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
--- exercises/07_threads/10_patch/src/lib.rs
+++ exercises/07_threads/10_patch/src/lib.rs
@@ -1,8 +1,7 @@
-use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
-
 // TODO: Implement the patching functionality.
 use crate::data::{Ticket, TicketDraft, TicketPatch};
 use crate::store::{TicketId, TicketStore};
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
 
 pub mod data;
 pub mod store;
@@ -35,12 +34,30 @@ impl TicketStoreClient {
         Ok(response_receiver.recv().unwrap())
     }
 
-    pub fn update(&self, ticket_patch: TicketPatch) -> Result<(), OverloadedError> {}
+    pub fn update(&self, ticket_patch: TicketPatch) -> Result<Ticket, UpdateError> {
+        let (response_sender, response_receiver) = sync_channel(1);
+        self.sender.try_send(
+            Command::Update {
+                patch: ticket_patch,
+                response_channel: response_sender,
+            }
+        ).map_err(|_| UpdateError {
+            msg: String::from("尝试更新失败")
+        })?;
+        response_receiver.recv().unwrap()
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
 #[error("The store is overloaded")]
 pub struct OverloadedError;
+
+#[derive(Debug, thiserror::Error)]
+#[error("更新错误:{msg}")]
+///更新错误
+pub struct UpdateError {
+    msg: String,
+}
 
 pub fn launch(capacity: usize) -> TicketStoreClient {
     let (sender, receiver) = sync_channel(capacity);
@@ -48,7 +65,7 @@ pub fn launch(capacity: usize) -> TicketStoreClient {
     TicketStoreClient { sender }
 }
 
-enum Command {
+pub enum Command {
     Insert {
         draft: TicketDraft,
         response_channel: SyncSender<TicketId>,
@@ -59,33 +76,53 @@ enum Command {
     },
     Update {
         patch: TicketPatch,
-        response_channel: SyncSender<()>,
+        response_channel: SyncSender<Result<Ticket, UpdateError>>,
     },
 }
+
 
 pub fn server(receiver: Receiver<Command>) {
     let mut store = TicketStore::new();
     loop {
         match receiver.recv() {
             Ok(Command::Insert {
-                draft,
-                response_channel,
-            }) => {
+                   draft,
+                   response_channel,
+               }) => {
                 let id = store.add_ticket(draft);
                 let _ = response_channel.send(id);
             }
             Ok(Command::Get {
-                id,
-                response_channel,
-            }) => {
+                   id,
+                   response_channel,
+               }) => {
                 let ticket = store.get(id);
                 let _ = response_channel.send(ticket.cloned());
             }
             Ok(Command::Update {
-                patch,
-                response_channel,
-            }) => {
-                todo!()
+                   patch,
+                   response_channel,
+               }) => {
+                let option = store.get_mut(patch.id);
+                let ticket = match option {
+                    None => {
+                        response_channel.send(Err(UpdateError { msg: String::from("没有查找到对应id的Ticket") })).unwrap();
+                        continue;
+                    }
+                    Some(tricket) => {
+                        tricket
+                    }
+                };
+                if let Some(status) = patch.status {
+                    ticket.status = status;
+                }
+                if let Some(title) = patch.title {
+                    ticket.title = title;
+                }
+                if let Some(des) = patch.description {
+                    ticket.description = des;
+                }
+                response_channel.send(Ok(ticket.clone())).unwrap()
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
